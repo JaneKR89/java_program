ca4ec44e9194d0b215e5f98ad42190061438ab1c
JaneKR89 <73488680+JaneKR89@users.noreply.github.com>
2020-10-28 13:31:22 -0400

    added few methods

--- sandbox/src/main/java/ru/stqa/ptf/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/ptf/sandbox/MyFirstProgram.java
@@ -2,10 +2,32 @@ package ru.stqa.ptf.sandbox;
 
 public class MyFirstProgram {
 
-  public static void main (String[]args){
+    public static void main(String[] args) {
 
-    System.out.println("Hello World!");
+        hello("world");
+        hello("user");
+        hello("Jane");
 
-}
+        double len = 5;
+        System.out.println("area of a square with a side " + len + " = " + area(len));
 
+        double a = 4;
+        double b = 5;
+        System.out.println("area of a rectangle with a sides " + a + " and " + b + " = " + area(a, b));
+
+
+    }
+
+    public static void hello(String somebody) {
+        System.out.println("Hello " + somebody + " !");
+    }
+
+    public static double area(double l) {
+        return l * l;
+    }
+
+    public static double area(double a, double b) {
+        return a * b;
+
+    }
 }
